a3feb687f5fd5a80ce4782e2c415be9a8be084f7
William Adama <damir.fattahov@gmail.com>
2012-04-21 21:19:51 +0600

    Форматирование кода контроллера Upload

--- app/controllers/upload.php
+++ app/controllers/upload.php
@@ -1,18 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Upload extends CI_Controller {
-   
+
    public function __construct()
-	{
-		parent::__construct();
-		
-		$this->load->helper('form');
-	}
-	
-	public function index()
-	{
-	   $this->load->view('upload_files');
-	}
+   {
+      parent::__construct();
+
+      $this->load->helper('form');
+   }
+
+   public function index()
+   {
+      $this->load->view('upload_files');
+   }
 
    public function add()
    {	
